1254dded6314f40c0ba6f47d81d7fe9e0f5d3744
Tamara_M <malahova-tamara@mail.ru>
2020-10-28 12:12:47 +0200

    Updates

---
+++ precache-manifest.85122cfdd54da8c31798d66dd5f6b071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e88dd78acb9f55732acaf4231db5e9",
+    "url": "/Pokemons_SPA/index.html"
+  },
+  {
+    "revision": "c62c38f01dadbc72cfa7",
+    "url": "/Pokemons_SPA/static/css/main.5d14741a.chunk.css"
+  },
+  {
+    "revision": "f35bed843eeae8ee0c56",
+    "url": "/Pokemons_SPA/static/js/2.37d6e7a5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Pokemons_SPA/static/js/2.37d6e7a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62c38f01dadbc72cfa7",
+    "url": "/Pokemons_SPA/static/js/main.3dac54cd.chunk.js"
+  },
+  {
+    "revision": "fd1b4ed9b2f17a7b7d27",
+    "url": "/Pokemons_SPA/static/js/runtime-main.17f6b8a4.js"
+  },
+  {
+    "revision": "691884b0090e34b694f0594c855a3d53",
+    "url": "/Pokemons_SPA/static/media/preloader.691884b0.svg"
+  }
+]);
